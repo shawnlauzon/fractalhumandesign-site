810fb088c5a63d7c80609ed97842a07688d385e0
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-09-05 16:33:23 -0500

    fix: useSearchParams() should be wrapped in a suspense boundary

--- src/app/get-birth-data/page.jsx
+++ src/app/get-birth-data/page.jsx
@@ -2,50 +2,40 @@
 
 import BirthDataForm from '@/components/BirthDataForm'
 import { useSearchParams } from 'next/navigation'
+import { Suspense } from 'react'
 
-const GetBirthDataPage = () => {
+const Admin = () => {
   const searchParams = useSearchParams()
   const isAdmin = searchParams.get('admin')
 
-  // const theme = window.matchMedia('(prefers-color-scheme: dark)').matches ? 'dark' : 'light';
+  return (
+    <>
+      {isAdmin && (
+        <div className="mt-4">
+          <p>Or enter a chart id here:</p>
+          <input
+            type="text"
+            value={chartId}
+            onChange={(e) => saveChart(e.target.value.trim())}
+            className="mt-2 w-full rounded border border-gray-300 p-2"
+          />
+        </div>
+      )}
+    </>
+  )
+}
 
-  const saveChart = (chartId) => {
-    setChartId(chartId)
-    if (chartId) {
-      if (searchParams.get('next') === 'leavejob') {
-        history.push({
-          pathname: '/leave-job-interview',
-          search: `?chart=${chartId}`,
-        })
-      } else {
-        history.push({
-          pathname: '/request-email',
-          search: `?chart=${chartId}`,
-        })
-      }
-    }
-  }
-
+export default function GetBirthDataPage() {
   return (
     <div className="bg-white text-gray-900 dark:bg-gray-900 dark:text-white">
       <main>
         <div className="container mx-auto p-4">
-          <BirthDataForm onChart={saveChart} />
-          {isAdmin && (
-            <div className="mt-4">
-              <p>Or enter a chart id here:</p>
-              <input
-                type="text"
-                value={chartId}
-                onChange={(e) => saveChart(e.target.value.trim())}
-                className="mt-2 w-full rounded border border-gray-300 p-2"
-              />
-            </div>
-          )}
+          <BirthDataForm />
+          <Suspense>
+            <Admin />
+          </Suspense>
         </div>
       </main>
     </div>
   )
 }
-
-export default GetBirthDataPage
