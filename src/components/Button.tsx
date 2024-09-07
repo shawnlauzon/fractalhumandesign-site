ed7908176d3a2c43b640ab4efa4096d2122bb225
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-09-07 15:11:16 -0500

    fix: Invalid DOM property `stroke-width`

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -71,7 +71,7 @@ export function Button({ className, ...props }: ButtonProps) {
           cy="12"
           r="10"
           stroke="currentColor"
-          stroke-width="4"
+          strokeWidth="4"
         ></circle>
         <path
           className="opacity-75"
