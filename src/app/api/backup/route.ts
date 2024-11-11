0500a59d98f5e6283aad3761dbdd16cc226960e3
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-11-10 18:02:50 -0600

    fix: Build fails with routes not a module

--- src/app/api/backup/route.ts
+++ src/app/api/backup/route.ts
@@ -1,61 +1,63 @@
-// export const dynamic = 'force-dynamic' // static by default, unless reading the request
+export const dynamic = 'force-dynamic' // static by default, unless reading the request
 
-// import { Client, fql, QuerySuccess } from 'fauna'
+export async function GET(req: Request) {
+  let httpResponse
 
-// export async function GET(req: Request) {
-//   let httpResponse
+  //   // configure your client
+  //   const client = new Client({
+  //     secret: process.env.FAUNADB_SERVER_SECRET,
+  //   })
 
-//   // configure your client
-//   const client = new Client({
-//     secret: process.env.FAUNADB_SERVER_SECRET,
-//   })
+  //   const backupClient = new Client({
+  //     secret: process.env.FAUNADB_BACKUP_SECRET,
+  //   })
 
-//   const backupClient = new Client({
-//     secret: process.env.FAUNADB_BACKUP_SECRET,
-//   })
+  //   try {
+  //     // Fetch users from the main client
+  //     const usersResponse: QuerySuccess<any> = await client.query(
+  //       fql`User.all().pageSize(200)`,
+  //     )
+  //     const users = usersResponse.data
+  //     console.log('users', users)
 
-//   try {
-//     // Fetch users from the main client
-//     const usersResponse: QuerySuccess<any> = await client.query(
-//       fql`User.all().pageSize(200)`,
-//     )
-//     const users = usersResponse.data
-//     console.log('users', users)
+  //     // Backup users to the backup client
+  //     for (const user of users.data) {
+  //       console.log('backing up user', user)
+  //       const resp = await backupClient.query(
+  //         fql`User.create({ id: ${user.id}, firstName: ${user.firstName}, lastName: ${user.lastName}, email: ${user.email} })`,
+  //       )
+  //       console.log('backup response', resp)
+  //     }
 
-//     // Backup users to the backup client
-//     for (const user of users.data) {
-//       console.log('backing up user', user)
-//       const resp = await backupClient.query(
-//         fql`User.create({ id: ${user.id}, firstName: ${user.firstName}, lastName: ${user.lastName}, email: ${user.email} })`,
-//       )
-//       console.log('backup response', resp)
-//     }
+  //     // Fetch charts from the main client
+  //     const chartsResponse: QuerySuccess<any> = await client.query(
+  //       fql`Chart.all().pageSize(200) { id, chart, user, meta }`,
+  //     )
+  //     const charts = chartsResponse.data
+  //     console.log('charts', charts)
 
-//     // Fetch charts from the main client
-//     const chartsResponse: QuerySuccess<any> = await client.query(
-//       fql`Chart.all().pageSize(200) { id, chart, user, meta }`,
-//     )
-//     const charts = chartsResponse.data
-//     console.log('charts', charts)
+  //     // Backup charts to the backup client
+  //     for (const chart of charts.data) {
+  //       console.log('backing up chart', chart)
+  //       const resp = await backupClient.query(
+  //         fql`Chart.create({ id: ${chart.id}, chart: ${chart.chart}, user: ${chart.user}, meta: ${chart.meta} })`,
+  //       )
+  //       console.log('backup response', resp)
+  //     }
 
-//     // Backup charts to the backup client
-//     for (const chart of charts.data) {
-//       console.log('backing up chart', chart)
-//       const resp = await backupClient.query(
-//         fql`Chart.create({ id: ${chart.id}, chart: ${chart.chart}, user: ${chart.user}, meta: ${chart.meta} })`,
-//       )
-//       console.log('backup response', resp)
-//     }
+  //     httpResponse = new Response(JSON.stringify({ users, charts }), {
+  //       status: 200,
+  //     })
+  //   } catch (error) {
+  //     httpResponse = new Response(JSON.stringify(error), { status: 500 })
+  //   } finally {
+  //     // clean up any remaining resources
+  //     client.close()
+  //     backupClient.close()
+  //   }
+  //   return httpResponse
 
-//     httpResponse = new Response(JSON.stringify({ users, charts }), {
-//       status: 200,
-//     })
-//   } catch (error) {
-//     httpResponse = new Response(JSON.stringify(error), { status: 500 })
-//   } finally {
-//     // clean up any remaining resources
-//     client.close()
-//     backupClient.close()
-//   }
-//   return httpResponse
-// }
+  return new Response(JSON.stringify({}), {
+    status: 404,
+  })
+}
--- src/app/api/bootstrap/route.ts
+++ src/app/api/bootstrap/route.ts
@@ -1,57 +1,59 @@
-// export const dynamic = 'force-dynamic' // static by default, unless reading the request
+export const dynamic = 'force-dynamic' // static by default, unless reading the request
 
-// import { Client, fql, QuerySuccess } from 'fauna'
+export async function GET(req: Request) {
+  //   let httpResponse
 
-// export async function GET(req: Request) {
-//   let httpResponse
+  //   // configure your client
+  //   const client = new Client({
+  //     secret: process.env.FAUNADB_SERVER_SECRET,
+  //   })
 
-//   // configure your client
-//   const client = new Client({
-//     secret: process.env.FAUNADB_SERVER_SECRET,
-//   })
+  //   type User = {
+  //     id: string
+  //     // emailToken?: string
+  //     isEmailVerified?: boolean
+  //     emailOptIn?: boolean
+  //   }
 
-//   type User = {
-//     id: string
-//     // emailToken?: string
-//     isEmailVerified?: boolean
-//     emailOptIn?: boolean
-//   }
+  // type Page = {
+  //   data: User[]
+  //   after: string
+  // }
 
-// type Page = {
-//   data: User[]
-//   after: string
-// }
+  //   try {
+  //     const users = []
+  //     // query using your app's local variables
+  //     const query = fql`User.all().pageSize(200) { id }
+  //     `
 
-//   try {
-//     const users = []
-//     // query using your app's local variables
-//     const query = fql`User.all().pageSize(200) { id }
-//     `
+  //     // execute the query
+  //     const queryResponse: QuerySuccess<Page> =
+  //       await client.query(query)
 
-//     // execute the query
-//     const queryResponse: QuerySuccess<Page> =
-//       await client.query(query)
+  //     for (const user of queryResponse.data.data) {
+  //       console.log('user', user)
+  //       // user.emailToken = uuidv4()
 
-//     for (const user of queryResponse.data.data) {
-//       console.log('user', user)
-//       // user.emailToken = uuidv4()
+  //       await client.query(
+  //         fql`User.byId(${user.id})!.update({emailOptIn: true, isEmailVerified: true })`,
+  //       )
 
-//       await client.query(
-//         fql`User.byId(${user.id})!.update({emailOptIn: true, isEmailVerified: true })`,
-//       )
+  //       users.push(user)
+  //       console.log('updated user', user)
+  //     }
 
-//       users.push(user)
-//       console.log('updated user', user)
-//     }
+  //     httpResponse = new Response(JSON.stringify(users), {
+  //       status: 200,
+  //     })
+  //   } catch (error) {
+  //     httpResponse = new Response(JSON.stringify(error), { status: 500 })
+  //   } finally {
+  //     // clean up any remaining resources
+  //     client.close()
+  //   }
+  //   return httpResponse
 
-//     httpResponse = new Response(JSON.stringify(users), {
-//       status: 200,
-//     })
-//   } catch (error) {
-//     httpResponse = new Response(JSON.stringify(error), { status: 500 })
-//   } finally {
-//     // clean up any remaining resources
-//     client.close()
-//   }
-//   return httpResponse
-// }
+  return new Response(JSON.stringify({}), {
+    status: 404,
+  })
+}
