0e88f191b70d1e8600e41a57d9bd736dbdb5919d
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-11-10 22:18:44 -0600

    fix: Don't send emails on email-open

--- src/app/api/email-open/route.ts
+++ src/app/api/email-open/route.ts
@@ -2,7 +2,6 @@ export const dynamic = 'force-dynamic' // static by default, unless reading the 
 
 import { User } from '@/types/User'
 import { Client, fql } from 'fauna'
-import * as postmark from 'postmark'
 
 export async function POST(req: Request) {
   let httpResponse
@@ -14,44 +13,15 @@ export async function POST(req: Request) {
   })
 
   try {
-    const thisUrl = 'https://' + req.headers.get('host')
-
-    const user = await client.query<User>(
-      fql`User.firstWhere(.isEmailVerified == false && .email == ${requestJson.Recipient})?.update({isEmailVerified: true }) { data }`,
+    await client.query<User>(
+      fql`User.firstWhere(.isEmailVerified == false && .email == ${requestJson.Recipient})?.update({isEmailVerified: true })`,
     )
-    console.log('updated user', user)
 
-    if (user) {
-      const postmarkClient = new postmark.ServerClient(
-        process.env.POSTMARK_API_KEY || '',
-      )
-
-      const emailData = {
-        From: 'shawn@fractalhumandesign.com',
-        To: requestJson.Recipient,
-        TemplateAlias: 'welcome',
-        TemplateModel: {
-          product_url: 'https://fractalhumandesign.com',
-          host_url: thisUrl,
-          product_name: 'Fractal Human Design',
-          name: user.data.firstName,
-          support_email: 'help@fractalhumandesign.com',
-          sender_name: 'Shawn Lauzon',
-          company_name: 'Quantum Connecting Technologies Inc',
-          company_address: '1800 W 68th St. Suite 118, Hialeah, FL  33014',
-        },
-      }
-      console.log('Sending', emailData)
-
-      const emailResponse =
-        await postmarkClient.sendEmailWithTemplate(emailData)
-      console.log('emailResponse', emailResponse)
-    }
-
-    httpResponse = new Response('', {
-      status: 201,
+    httpResponse = new Response(requestJson.Recipient, {
+      status: 200,
     })
   } catch (error) {
+    console.error('Error', error)
     httpResponse = new Response(JSON.stringify(error), { status: 500 })
   } finally {
     client.close()
