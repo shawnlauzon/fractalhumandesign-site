da176c3b022f540692d5f78fbc3afdf1c6ac0b2b
Shawn Lauzon <shawn.lauzon@gmail.com>
2024-11-12 10:30:30 -0600

    fix: Wrong color on annotated chart

--- src/components/AnnotatedChart.tsx
+++ src/components/AnnotatedChart.tsx
@@ -44,7 +44,7 @@ export default async function AnnotatedChart({ chartId }: AnnotatedChartProps) {
                   <a
                     href="https://calendly.com/fractalhumandesign/bg5overview"
                     target="_blank"
-                    className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                    className="rounded-md bg-sky-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-sky-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-600"
                   >
                     Or schedule a $97 session with me right away
                   </a>
